ee5a7c9bb40b6451650daf114daab2158bcd27f0
Jesus Mogollon <beto-barca10@hotmail.com>
2020-09-16 19:07:10 -0400

    leer json firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = []
 
   constructor(private http: HttpClient) {
-    //console.log('info pagina cargada');
 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+  private cargarInfo() {
     // Leer el archivo json
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
+        //console.log(resp);
+      })
+  }
+  private cargarEquipo() {
+    // Leer el archivo json
+    this.http.get('https://angular-html-29357.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+
+
+        this.equipo = resp;
         console.log(resp);
       })
-
   }
 }
